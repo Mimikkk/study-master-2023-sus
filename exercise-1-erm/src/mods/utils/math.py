d4d1b51e440a7a5bf3d5bf44007e46718102c9d5
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-03-06 15:21:45 +0100

    add information gain decision

--- exercise-1-erm/src/mods/utils/math.py
+++ exercise-1-erm/src/mods/utils/math.py
@@ -1,9 +1,18 @@
 from typing import Iterable
 
 from numpy import ndarray, var
+from scipy.stats import entropy
+
 def variance_gain(common: Iterable[ndarray[float]],
                   left: Iterable[ndarray[float]],
                   right: Iterable[ndarray[float]]):
   left_p = len(left) / len(common)
   right_p = len(right) / len(common)
   return var(common) - left_p * var(left) - right_p * var(right)
+
+def information_gain(common: Iterable[ndarray[float]],
+                     left: Iterable[ndarray[float]],
+                     right: Iterable[ndarray[float]]):
+  left_p = len(left) / len(common)
+  right_p = len(right) / len(common)
+  return entropy(common) - left_p * entropy(left) - right_p * entropy(right)
