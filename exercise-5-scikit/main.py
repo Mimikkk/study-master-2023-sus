babbeb105a8bcfa147494e2c4649750a761e2dc0
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-04-14 21:15:08 +0200

    model scores

--- exercise-5-scikit/main.py
+++ exercise-5-scikit/main.py
@@ -2,12 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import sklearn.ensemble
+from sklearn import linear_model
+from sklearn import neighbors
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+import sklearn.tree as trees
+import sklearn.ensemble as ensemble
 
 
-def main():
+def read():
   columns = np.genfromtxt('resources/datasets/regression.txt', dtype=str, max_rows=1)
   rows = np.genfromtxt('resources/datasets/regression.txt', dtype=np.float32, skip_header=1)
-  frame = pd.DataFrame(data=rows, columns=columns)
+  return pd.DataFrame(data=rows, columns=columns)
+
+
+def analysis(frame: pd.DataFrame):
   # Analysis
   print(f"Number of attributes: {frame.shape[1] - 1}")
   print(f"Number of rows: {frame.shape[0]}")
@@ -42,6 +54,79 @@ def main():
   plt.tight_layout()
   plt.savefig(f'resources/figures/correlation-matrix.png')
   plt.show()
+
+
+def test_models(frame: pd.DataFrame):
+  contents = frame.to_numpy()
+
+  X = contents[:, 0:-1]
+  y = contents[:, -1]
+
+  from sklearn.metrics import mean_squared_error
+
+  def calculate_metrics(regressor, X, y):
+    return regressor.score(X, y), mean_squared_error(y, regressor.predict(X), squared=False)
+
+  def format_scores(title, r2, rmse):
+    return f"{title: >20}:\n\tR2: {r2:.2f} | RMSE: {rmse:.2f}"
+
+  scores = []
+  for (name, regressor) in [
+    ("Random Forest", RandomForestRegressor()),
+    ("Linear Regression", linear_model.LinearRegression()),
+    ("Ridge", linear_model.Ridge()),
+    ("Lasso", linear_model.Lasso()),
+    ("Elastic Net", linear_model.ElasticNet()),
+    ("Bayesian Ridge", linear_model.BayesianRidge()),
+    ("Knn-1", neighbors.KNeighborsRegressor(n_neighbors=1)),
+    ("Knn-2", neighbors.KNeighborsRegressor(n_neighbors=2)),
+    ("Knn-3", neighbors.KNeighborsRegressor(n_neighbors=3)),
+    ("Knn-4", neighbors.KNeighborsRegressor(n_neighbors=4)),
+    ("Knn-8", neighbors.KNeighborsRegressor(n_neighbors=8)),
+    ("Knn-13", neighbors.KNeighborsRegressor(n_neighbors=13)),
+    ("Knn-21", neighbors.KNeighborsRegressor(n_neighbors=21)),
+    ("Tree-2", DecisionTreeRegressor(max_depth=2)),
+    ("Tree-5", DecisionTreeRegressor(max_depth=5)),
+    ("Tree-8", DecisionTreeRegressor(max_depth=8)),
+    ("Tree-unbound", DecisionTreeRegressor(max_depth=None)),
+    ("Pony", MLPRegressor()),
+    ("svr-rbf", SVR(kernel="rbf")),
+    ("svr-linear", SVR(kernel="linear")),
+    ("svr-poly", SVR(kernel="poly")),
+    ("ard", linear_model.ARDRegression()),
+    ("sgd", linear_model.SGDRegressor()),
+    ("adaboost", ensemble.AdaBoostRegressor()),
+    ("gradientboosting", ensemble.GradientBoostingRegressor()),
+  ]:
+    regressor.fit(X, y)
+    scores.append((name, calculate_metrics(regressor, X, y)))
+
+  for name, (r2, rmse) in scores: print(format_scores(name, r2, rmse))
+
+  r2_scores = [r2 for _, (r2, _) in scores]
+  rmse_scores = [rmse for _, (_, rmse) in scores]
+  figure, axes = plt.subplots(1, 2, figsize=(32, 8))
+  sns.barplot(x=[name for name, _ in scores], y=r2_scores, ax=axes[0])
+  axes[0].set_title("R2 Scores")
+  axes[0].set_ylabel("R2 Score")
+  axes[0].set_xticklabels(labels=[name for name, _ in scores], rotation=60)
+
+  sns.barplot(x=[name for name, _ in scores], y=rmse_scores, ax=axes[1])
+  axes[1].set_title("RMSE Scores")
+  axes[1].set_ylabel("RMSE Score")
+  axes[1].set_xticklabels(labels=[name for name, _ in scores], rotation=60)
+
+  plt.tight_layout()
+  plt.savefig(f'resources/figures/model-scores.png')
+  plt.show()
+
+
+def main():
+  frame = read()
+
+  # analysis(frame)
+
+  test_models(frame)
 
 
 if __name__ == '__main__':
