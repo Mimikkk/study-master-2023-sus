f73fc706fe5be218c5ef4826bc022a3562c4ac1a
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-04-22 17:33:36 +0200

    plots

--- exercise-6-weka/main.py
+++ exercise-6-weka/main.py
@@ -4,9 +4,7 @@ from matplotlib import pyplot as plt
 if __name__ == '__main__':
   csv = './results.csv'
   df = pd.read_csv(csv)
-  print(df)
 
-  x = range(len(df))
   df.plot.line(y='r2', use_index=True)
   df.plot.line(y='rmse', use_index=True)
   plt.show()
