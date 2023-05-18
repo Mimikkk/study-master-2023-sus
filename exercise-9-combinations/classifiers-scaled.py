b409e2db96255375584a2672b22e4e2601ecc0ae
Hououin Kyouma <inwazzin@gmail.com>
2023-05-19 00:11:44 +0200

    init data, task1-task2-task3

---
+++ exercise-9-combinations/classifiers-scaled.py
@@ -0,0 +1,106 @@
+from imblearn.metrics import geometric_mean_score
+from pandas import DataFrame
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.metrics import make_scorer, roc_auc_score, accuracy_score, confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, VotingClassifier, StackingClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import matplotlib.pyplot as plt
+import dataset as ds
+
+def gmean():
+  return make_scorer(geometric_mean_score)
+
+def main():
+  df: DataFrame = ds.load()
+  X, y = df.values[:, :-1], df.values[:, -1]
+
+  classifiers = [
+    AdaBoostClassifier(estimator=SVC(probability=True, kernel='linear')),
+    AdaBoostClassifier(estimator=DecisionTreeClassifier()),
+    RandomForestClassifier(),
+    VotingClassifier(estimators=[
+      ('dt', DecisionTreeClassifier()),
+      ('svc', SVC()),
+      ('mlp', MLPClassifier())
+    ]),
+    VotingClassifier(estimators=[
+      ('gnb', GaussianNB()),
+      ('qda', QuadraticDiscriminantAnalysis()),
+      ('rf', RandomForestClassifier())
+    ]),
+    VotingClassifier(estimators=[
+      ('ada', AdaBoostClassifier()),
+      ('dt', DecisionTreeClassifier()),
+      ('svc', SVC())
+    ]),
+    StackingClassifier(estimators=[
+      ('dt', DecisionTreeClassifier()),
+      ('svc', SVC()),
+      ('mlp', MLPClassifier())
+    ]),
+    StackingClassifier(estimators=[
+      ('gnb', GaussianNB()),
+      ('qda', QuadraticDiscriminantAnalysis()),
+      ('rf', RandomForestClassifier())
+    ]),
+    StackingClassifier(estimators=[
+      ('ada', AdaBoostClassifier()),
+      ('dt', DecisionTreeClassifier()),
+      ('svc', SVC())
+    ])
+  ]
+  cv = StratifiedKFold(n_splits=10)
+
+  metrics = []
+  for classifier in classifiers:
+    print(f'Classifier: {classifier.__class__.__name__}')
+
+    pipeline = Pipeline([
+      ('Classifier', classifier)
+    ])
+
+    print(f'Accuracy')
+    accuracy = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv).mean()
+    print(f'G-mean')
+    gmean_score = cross_val_score(pipeline, X, y, scoring=gmean(), cv=cv).mean()
+    metrics.append((accuracy, gmean_score))
+
+  labels = [
+    'AdaBoost-SVC',
+    'AdaBoost-DT',
+    'R-Forest',
+    'Voting-1',
+    'Voting-2',
+    'Voting-3',
+    'Stacking-1',
+    'Stacking-2',
+    'Stacking-3'
+  ]
+
+  x = np.arange(len(labels))
+  width = 0.2
+
+  accuracy, g_mean = zip(*metrics)
+  fig, ax = plt.subplots()
+  ax.bar(x - width, accuracy, width, label='Trafność')
+  ax.bar(x + width, g_mean, width, label='G-Mean')
+  ax.set_ylabel('Wyniki')
+  ax.set_title('Wynik względem klasyfikatora i metryki')
+  ax.set_xticks(x)
+  ax.set_xticklabels(labels)
+  ax.legend()
+
+  fig.tight_layout()
+  plt.show()
+
+if __name__ == '__main__':
+  main()
---
+++ exercise-9-combinations/exploration.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+import dataset as ds
+import seaborn as sns
+
+def main():
+  df = ds.load()
+  print(df.info())
+  print(df.describe())
+
+  plt.figure(figsize=(120, 16))
+  sns.boxplot(data=df)
+  plt.xticks(rotation=90)
+  plt.show()
+  # count classes
+  print(df['class'].value_counts())
+
+if __name__ == '__main__':
+  main()
